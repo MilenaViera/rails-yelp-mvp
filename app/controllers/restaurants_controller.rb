f405a26ae203201208e13f4afcf365503227242e
mile <milenajohanna1@gmail.com>
2022-02-16 18:04:13 -0300

    segundo ejercicio

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,27 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_creator)
+    @restaurant.save
+    redirect_to restaurants_path
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  private
+
+  def restaurant_creator
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "Gourmet Porteño", address: "Buenos Aires", phone_number: 1234567891,category: "italian")
+Restaurant.create(name: "la cabrera", address: "Buenos Aires", phone_number: 1234567891,category: "italian")
+Restaurant.create(name: "Donato", address: "Buenos Aires", phone_number: 1234567891,category: "italian")
+Restaurant.create(name: "Kansas", address: "Buenos Aires", phone_number: 1234567891,category: "italian")
+Restaurant.create(name: "Amazonas", address: "Buenos Aires", phone_number: 1234567891,category: "italian")
